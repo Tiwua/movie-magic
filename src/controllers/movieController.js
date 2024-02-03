4ea35fefdeeb8bb781f198b181887a4fb60345d8
Valentin Gaidarov <valentin.gaidarov@gmail.com>
2024-02-03 23:19:31 +0200

    Cast Added

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const movieService = require('../services/movieService');
+const castService = require('../services/castService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -28,8 +29,11 @@ router.get('/movies/:movieId', async (req, res) => {
     res.render('details', { movie });
 });
 
-router.get('/movies/:movieId/attach', (req, res) => {
-    res.render('movie/attach')
+router.get('/movies/:movieId/attach', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId).lean();
+    const casts = await castService.getAll().lean();
+
+    res.render('movie/attach', {...movie, casts})
 });
 
 module.exports = router;
